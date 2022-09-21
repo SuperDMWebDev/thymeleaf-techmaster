b40b2e1308f9f443609d8f8e4502fa19efc3f700
DMSofwareDev <89517604+SuperDMWebDev@users.noreply.github.com>
2022-09-21 10:14:10 +0700

    first commit

---
+++ src/main/java/com/example/spring8techmaster/Spring8TechmasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring8techmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring8TechmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring8TechmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring8techmaster/Spring8TechmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring8techmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring8TechmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
